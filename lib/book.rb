a6595bc5682b44ff06d772af5b9711d8ba72514e
Elijah Silverman <elijahmsilverman@gmail.com>
2021-11-23 11:05:05 -0500

    commit tree 07d5b40d058630138d56c3ee3326ee671da4ffc8

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book 
+
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
 
+end
+
+
+book = Book.new("And Then There Were None")
+book.author= "Agatha Christie"
+book.page_count= 272
+book.genre= "Mystery"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+
+class Shoe 
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @cobble = puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+
+    # attr_reader :cobble
+    attr_reader :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+end
+
+shoe = Shoe.new("Adidas")
+shoe.color = "red"
+shoe.size = 9.6
+shoe.material = "suede"
+shoe.condition = "tattered"
